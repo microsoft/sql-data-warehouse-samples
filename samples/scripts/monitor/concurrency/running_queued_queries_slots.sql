cff7e68904a238818fc00d72e009596c3e6bb0bf
Matt Usher <mausher@microsoft.com>
2018-03-08 14:19:48 -0800

    Updating the running_queued_queries_slots query for proper labels

--- samples/scripts/monitor/concurrency/running_queued_queries_slots.sql
+++ samples/scripts/monitor/concurrency/running_queued_queries_slots.sql
@@ -1,9 +1,9 @@
 -- Total running queries and slots consumed
 SELECT
-	RunningQueries  = SUM(CASE WHEN r.[status] ='Running'   THEN 1 ELSE 0 END)
-	,SlotsGranted   = SUM(CASE WHEN r.[status] ='Running'   THEN rw.concurrency_slots_used ELSE 0 END)
-	,QueuedQueries  = SUM(CASE WHEN r.[status] ='Suspended' THEN 1 ELSE 0 END)
-	,SlotsQueued    = SUM(CASE WHEN rw.[state] ='Queued'    THEN rw.concurrency_slots_used ELSE 0 END)
+	SUM(CASE WHEN r.[status] ='Running'   THEN 1 ELSE 0 END)							[running_queries],
+	SUM(CASE WHEN r.[status] ='Running'   THEN rw.concurrency_slots_used ELSE 0 END)	[running_queries_slots],
+	SUM(CASE WHEN r.[status] ='Suspended' THEN 1 ELSE 0 END)							[queued_queries],
+	SUM(CASE WHEN rw.[state] ='Queued'    THEN rw.concurrency_slots_used ELSE 0 END)	[queued_queries_slots]
 FROM
 	sys.dm_pdw_exec_requests r 
 	JOIN sys.dm_pdw_resource_waits rw on rw.request_id = r.request_id
