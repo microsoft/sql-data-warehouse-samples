98adce1cdedf3c0b5b39d8561a781aaddc6780cb
Matt Usher <mausher@microsoft.com>
2018-06-13 02:11:07 -0700

    Adding the slot usage query

--- samples/sqlops/MonitoringScripts/LatestBackup.sql
+++ samples/sqlops/MonitoringScripts/LatestBackup.sql
@@ -1,4 +1,10 @@
 /* Latest backup deatils */
-select   top 1 start_time, end_time, progress AS progress_percent
-from     sys.pdw_loader_backup_runs 
-order by run_id desc
+SELECT TOP 1
+	start_time									[start_time]
+	, end_time									[end_time]
+	, progress									[progress_percent]
+	, DATEDIFF(SECOND, start_time, end_time)	[duration_seconds]
+FROM
+	[sys].[pdw_loader_backup_runs]
+ORDER BY
+	[run_id] DESC;
---
+++ samples/sqlops/MonitoringScripts/SlotUsage.sql
@@ -0,0 +1,12 @@
+-- Total running queries and slots consumed
+SELECT
+	SUM(CASE WHEN r.[status] ='Running'   THEN 1 ELSE 0 END)							[running_queries]
+	, SUM(CASE WHEN r.[status] ='Running'   THEN rw.concurrency_slots_used ELSE 0 END)	[running_queries_slots]
+	, SUM(CASE WHEN r.[status] ='Suspended' THEN 1 ELSE 0 END)							[queued_queries]
+	, SUM(CASE WHEN rw.[state] ='Queued'    THEN rw.concurrency_slots_used ELSE 0 END)	[queued_queries_slots]
+FROM
+	[sys].[dm_pdw_exec_requests] r 
+	JOIN [sys].[dm_pdw_resource_waits] rw ON rw.request_id = r.request_id
+WHERE
+	( (r.[status] = 'Running' AND r.[resource_class] IS NOT NULL ) OR r.[status] ='Suspended' )
+	AND rw.[type] ='UserConcurrencyResourceType';
